7401a75b3e0ff9183e5500c291c530bd4d4e74b0
SAnschutz <23403170+SAnschutz@users.noreply.github.com>
2019-08-29 00:04:55 -0400

    further tweaks to start scripts and static assets

--- server/app.js
+++ server/app.js
@@ -26,11 +26,20 @@ app.use(jarRouter);
 app.use(jobRouter);
 
 //Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//   //Set static folder
+//   app.use(express.static(__dirname + '/client/build'));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 if (process.env.NODE_ENV === 'production') {
-  //Set static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
