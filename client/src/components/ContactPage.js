7949a22b740d4397c3565020951817808edd807f
SAnschutz <23403170+SAnschutz@users.noreply.github.com>
2019-09-03 01:51:28 -0400

    mobile styling about, contact, navbar menus

--- client/src/components/ContactPage.js
+++ client/src/components/ContactPage.js
@@ -9,19 +9,20 @@ export default function ContactPage() {
       <div id='contact-page'>
         <h1>Contact Page</h1>
         <p>
-          Want to contact us, to let us know about an issue, tell us an amusing
-          anecdote, or, better yet, tell us an inspiring story about how JobJar
+          Want to contact me, to let me know about an issue, tell me an amusing
+          anecdote, or, better yet, tell me an inspiring story about how JobJar
           has changed your life for the better and you couldn't possibly go back
           to living without it?? &ensp;
           <span>&#128522;</span>
         </p>
         <p>
-          Send us an e-mail at{' '}
+          Send me an e-mail at{' '}
           <a href='mailto: contactjobjar@gmail.com' className='email-link'>
             contactjobjar@gmail.com
           </a>
-          , and we'll get back to you just as soon as we finish our current
-          JobJar task.
+        </p>
+        <p>
+          I'll get back to you just as soon as I finish my current JobJar task.
         </p>
         <Link to='/'>Go back</Link>
       </div>
