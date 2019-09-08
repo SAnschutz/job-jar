2e04f440cdd96671122b18bdec245ffa91bed074
SAnschutz <23403170+SAnschutz@users.noreply.github.com>
2019-09-08 01:04:05 -0400

    changes to node routes for production

--- server/app.js
+++ server/app.js
@@ -27,17 +27,18 @@ app.use(jobRouter);
 //Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   //Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  // ''client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 // Serve any static files
-app.use(express.static(__dirname + 'client/build'));
+// app.use(express.static(__dirname + 'client/build'));
 
-// Handle React routing, return all requests to React app
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// // Handle React routing, return all requests to React app
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 module.exports = app;
