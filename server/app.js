5a71eda790601526f1c85576245693b29d0f4b8b
SAnschutz <23403170+SAnschutz@users.noreply.github.com>
2019-09-08 01:15:04 -0400

    heroku route issue

--- server/app.js
+++ server/app.js
@@ -34,11 +34,11 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 // Serve any static files
-// app.use(express.static(__dirname + 'client/build'));
+app.use(express.static(__dirname + 'client/build'));
 
-// // Handle React routing, return all requests to React app
-// app.get('*', function(req, res) {
-//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 module.exports = app;
